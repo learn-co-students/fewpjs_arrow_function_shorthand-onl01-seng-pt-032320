7505143429062406a6aaebec1261d43ccd0813fe
pledgedavis <pledgedavis14@gmail.com>
2020-10-25 01:53:13 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// let divide = (param1, param2) => param1/param2
+// divide(2000, 100)
+
+// let square = (param, param2)
+
+// let add = (param1, param2)
+
+let divide = () => {
+    return 2000 / 100
+}
+
+let square = num => { 
+    return num * num
+}
+
+
+let add = (param1, param2) => {
+    return param1 + param2
+}
